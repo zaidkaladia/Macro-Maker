1b703ad217a594957a63d881b9af14c6195a81cb
Deep-Kumawat <deepdkmail@gmail.com>
2022-03-26 07:24:48 +0530

    now it works even for multi macro, keypress then click etc

--- zaidAndDeepMiniProjectV0.py
+++ zaidAndDeepMiniProjectV0.py
@@ -11,38 +11,7 @@ import time
 import pyautogui
 import keyboard
 # end of imports-----------------------
-# Implementation of object oriented programming---------------------------------
 # i want to some how store the action in a list or something
-
-class ActionElement:  # this is the class for every action type, like, mouse click, double click, keypress..,.. everthing.#this would hold the squence of action, macro key with which they are tied.
-    squenceNumber = None
-    # this is the macro key that the key action is tied to.
-    macroKeyThatExecutesMe = None
-    pass
-
-class MouseSingleClick(ActionElement):
-    def __init__(self, XCoordinate=None, YCoordinate=None):
-        self.Xcoordinate = XCoordinate
-        self.Ycoordinate = YCoordinate
-
-    def click(self):
-        XorignalCursorPosition = pyautogui.position().x #originalCursorPosition is the postition of mouse before the mouse moved to record the macro
-        YorignalCursorPosition = pyautogui.position().y #originalCursorPosition is the postition of mouse before the mouse moved to record the macro
-        pyautogui.click(self.Xcoordinate, self.Ycoordinate)
-        pyautogui.moveTo(XorignalCursorPosition, YorignalCursorPosition)
-
-class MouseDoubleClick(MouseSingleClick):
-    # def __init__(self, XCoordinates = None, YCoordinates = None):
-    #     self.Xcoordinate = XCoordinates
-    #     self.Ycoordinate = YCoordinates
-    def __init__(self, XCoordinate=None, YCoordinate=None):
-        super().__init__(XCoordinate, YCoordinate)
-
-    def click(self):
-        XorignalCursorPosition = pyautogui.position().x #originalCursorPosition is the postition of mouse before the mouse moved to record the macro
-        YorignalCursorPosition = pyautogui.position().y #originalCursorPosition is the postition of mouse before the mouse moved to record the macro
-        pyautogui.doubleClick(self.Xcoordinate, self.Ycoordinate)
-        pyautogui.moveTo(XorignalCursorPosition, YorignalCursorPosition)
 
 # -------------------initializations-----------------------------------------------------------
 listOfActions = []
@@ -58,9 +27,12 @@ while userChoiceToMakeNewMacrosOrUseExisting != 0:
         print("waiting for the macro key to be pressed...")
         keyboard.wait(inputMacroKey)
         for i in listOfActions:
-            if i == singleClickActionObject:
-                i.click()
-        pass
+            if i[0] == "MouseSingleClick":
+                pyautogui.click(i[1], i[2])
+            elif i[0] == "MouseDoubleClick":
+                pyautogui.doubleClick(i[1], i[2])
+            elif i[0] == "Keypress":
+                keyboard.press(i[1])
     elif userChoiceToMakeNewMacrosOrUseExisting == 2:  # 2 to create new macros
         print("press the key to which you want to map the action and then press the escape key:")
         inputMacroKey = keyboard.record(until='esc')
@@ -76,51 +48,32 @@ while userChoiceToMakeNewMacrosOrUseExisting != 0:
                 break
             while newMacroActionType_UserInput != 0:
                 if newMacroActionType_UserInput == 1:  # choice for single click
-                    singleClickActionObject = MouseSingleClick()
-                    listOfActions.append(singleClickActionObject)
                     input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
                     for i in range(1, input_delay+1):
                         print("tik tik", i)
                         time.sleep(1)
-                    singleClickActionObject.Xcoordinate =  pyautogui.position().x
-                    singleClickActionObject.Ycoordinate =  pyautogui.position().y
-                    #~~~~~~~~~~~~~~~Object created and after calling click method, it should perform the click~~~~~~~~~~~~~~~~~~#
-                    # print("waiting for macro key to be pressed...")
-                    # keyboard.wait(inputMacroKey)
-                    # singleClickActionObject.click()
-
+                    listForMouseSingleClick = ["MouseSingleClick", pyautogui.position().x, pyautogui.position().y]
+                    listOfActions.append(listForMouseSingleClick)
                     break
-                elif newMacroActionType_UserInput == 2:
+                elif newMacroActionType_UserInput == 2:# Choice For Double Click
                     #here goes the code for double click
-                    doubleClickActionObject = MouseDoubleClick()
                     input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
                     for i in range(1, input_delay+1):
                         print("tik tik", i)
                         time.sleep(1)
-                    doubleClickActionObject.Xcoordinate =  pyautogui.position().x
-                    doubleClickActionObject.Ycoordinate =  pyautogui.position().y
-                    #~~~~~~~~~~~~~~~Object created and after calling click method, it should perform the click~~~~~~~~~~~~~~~~~~#
-                    print("waiting for macro key to be pressed...")
-                    keyboard.wait(inputMacroKey)
-                    doubleClickActionObject.click()
-                    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+                    listForMouseDoubleClick = ["MouseDoubleClick", pyautogui.position().x, pyautogui.position().y]
+                    listOfActions.append(listForMouseDoubleClick)
                     break
                 elif newMacroActionType_UserInput == 3:
-                    # here goes the code for keypress
-                    print("Press the macro key for the action(press escape after pressing the key)")
-                    inputMacroKey = keyboard.record(until='esc')
-                    inputMacroKey = str(inputMacroKey[0])
-                    inputMacroKey = inputMacroKey[14:-6]
                     print("Press the key you want to be pressed with the macro(press escape after pressing the key)")
+                    time.sleep(1)
                     actionKey = keyboard.record(until='esc')#its the key the user wants the program to press after he presses the macro key
                     actionKey = str(actionKey[0])
+                    print(actionKey)
                     actionKey = actionKey[14:-6]
                     print("The key you pressed was --->", actionKey)
-                    print("Your selected macro key was --->", inputMacroKey)
-                    print("waiting for macro key to be pressed...")
-                    keyboard.wait(inputMacroKey)
-                    pyautogui.press(actionKey)
-                    print("Action executed successfully")
+                    listForKeypresses = ["Keypress", actionKey]
+                    listOfActions.append(listForKeypresses)
                 break
     else:#for an invalid input
         print("\nInvalid input try again!\n")
